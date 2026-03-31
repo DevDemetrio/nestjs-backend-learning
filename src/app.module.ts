cd611eb540e3817cbfef817b43fd017f6e3a95e0
Demétrio Nascimento <devdemetrio@gmail.com>
2026-03-31 17:06:11 -0400

    adding modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
-import { ProductsService } from './products/products.service';
-import { AlunoDemetrio, ConhecimentoFaculdade } from './products/products.controller';
+// import { ProductsModule } from './products/products.module';
+// import { ProductsService } from './products/products.service';
+// import { AlunoDemetrio, ConhecimentoFaculdade } from './products/products.controller';
 import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [ProductsModule, ClientsModule],
+  imports: [ ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
