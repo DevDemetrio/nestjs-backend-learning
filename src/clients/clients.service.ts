b0585f1f127b1056e26c323c253d3d7dcc43d566
Demétrio Nascimento <devdemetrio@gmail.com>
2026-03-31 17:05:43 -0400

    adding service

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,18 +1,48 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
 export class ClientsService {
+
+    private products: any[] = []
     findAll(){
-        return ["Demetrio", "Thamara", "Melinda"]
+        return this.products
     }
 
-    postClients(name:string, age: number){
-        if(age < 18){
-            return {message: 'Usuário menor de idade'}
+    postProduct(product: { name: string; price: number; description: string }){
+        const newProduct = {
+            id: Date.now(),
+            ...product
         }
-        if(!name){
-            return {message: 'Precisa preencher nome'}
+        this.products.push(newProduct)
+        return newProduct
+    }
+
+    getById(id: number){
+        return this.products.find(item => item.id === id)
+    }
+
+    delete(id: number){
+        const product = this.products.find(item => item === id)
+        if(!product) {
+            return {error: "Produto não encontrado"}
         }
-        return { message: `Usuario ${name} com ${age} anos criados`}
+        this.products = this.products.filter(item => item !== id)
+        
+        return {message: "Produto removido com sucesso"}
+    }
+
+    update(id: number, body: {name: string; price: number; description: string}){
+        const product = this.products.find(item => item.id === id)
+
+        if(!product){
+            return {error: "Produto não encontrado"}
+        }
+
+       product.name = body.name
+       product.price = body.price
+       product.description = body.description
+
+       return product
     }
 }
